949e67b8bd42a00b489866e1fdc0a33a6119012f
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-02-25 14:17:33 -0300

    chore: adding and removing elements

---
+++ js/example4.js
@@ -0,0 +1,11 @@
+var app4 = angular.module('app4', []); 
+
+app4.controller('eventCtrl', function($scope) {
+
+//     $scope.blur = 0;
+//     $scope.click = 0;
+// });
+
+$scope.disableButton = true; 
+
+});
